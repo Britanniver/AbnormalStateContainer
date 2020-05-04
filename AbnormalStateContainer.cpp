bbf9ae2f4629007f5e8817f4847ad4ec2c6e5d07
Britanniver <Britanniver@users.noreply.github.com>
2020-05-04 17:12:09 +0200

    tidied up

--- AbnormalStateContainer.cpp
+++ AbnormalStateContainer.cpp
@@ -1,22 +1,18 @@
 void __thiscall AbnormalStateContainer::AbstateListInObject::AbstateListInObject(AbnormalStateContainer::AbstateListInObject *this) {
 
-  AbnormalStateContainer::AbstateListInObject *this; // edi
   List<AbnormalStateContainer::AbstateElementInObject> listAbstateObject; // esi
   AbnormalStateContainer::AbstateElementInObject abstateElement; // edi
 
   int innerIndex; // eax
-  signed int outerIndex; // ecx
+  int outerIndex; // ecx
 
   this->asl_Element = new AbnormalStateContainer::AbstateElementInObject[0x1C);
+                                                                         
   listAbstateObject = &this->asl_List;
-
   listAbstateObject->vfptr = &List<AbnormalStateContainer::AbstateElementInObject>::vftable;
   listAbstateObject->l_ListArray = 0; // +16
 
   List<AbnormalStateContainer::AbstateElementInObject>::l_MakeList(listAbstateObject, 0x1C);
-
-  int structSize = sizeof(AbnormalStateContainer::AbstateElementInObject); // 116
-  int innerStructSize = sizeof(ListStruct<AbnormalStateContainer::AbstateElementInObject>) // 12
 
   innerIndex = 0;
   outerIndex = 2;
@@ -26,29 +22,28 @@ void __thiscall AbnormalStateContainer::AbstateListInObject::AbstateListInObject
 
     if ( outerIndex - 2 < listAbstateObject->l_MaxSize) {
       if (!listAbstateObject->l_Array[innerIndex])
-        listAbstateObject->l_Array[innerIndex] = &this->asl_Element[outerIndex - 2]
+        listAbstateObject->l_Array[innerIndex].ls_Content = &this->asl_Element[outerIndex - 2]
     }
 
     if ( outerIndex - 1 < listAbstateObject->l_MaxSize) {
       if (listAbstateObject->l_Array[innerIndex + 1] == 0)
-        listAbstateObject->l_Array[innerIndex + 1] = &this->asl_Element[outerIndex - 1]
+        listAbstateObject->l_Array[innerIndex + 1].ls_Content = &this->asl_Element[outerIndex - 1]
     }
 
     if ( outerIndex < listAbstateObject->l_MaxSize) {
       if (listAbstateObject->l_Array[innerIndex + 2] == 0)
-        listAbstateObject->l_Array[innerIndex + 2] = &this->asl_Element[outerIndex]
+        listAbstateObject->l_Array[innerIndex + 2].ls_Content = &this->asl_Element[outerIndex]
     }
 
     if ( outerIndex + 1 < listAbstateObject->l_MaxSize) {
       if (listAbstateObject->l_Array[innerIndex + 3] == 0)
-        listAbstateObject->l_Array[innerIndex + 3] = &this->asl_Element[outerIndex + 1]
+        listAbstateObject->l_Array[innerIndex + 3].ls_Content = &this->asl_Element[outerIndex + 1]
     }
 
     innerIndex += 4;
     outerIndex += 4;
 
     abstateElement = &this->asl_Element[outerIndex];
-
   }
   while ( outerIndex - 2 < 28 );
 }
