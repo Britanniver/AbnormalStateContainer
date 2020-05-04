110819b5a3a41bc68f0eb7a97c90e4b34ac54b33
Britanniver <Britanniver@users.noreply.github.com>
2020-05-04 17:15:09 +0200

    Minified

--- AbnormalStateContainer.cpp
+++ AbnormalStateContainer.cpp
@@ -1,10 +1,6 @@
 void __thiscall AbnormalStateContainer::AbstateListInObject::AbstateListInObject(AbnormalStateContainer::AbstateListInObject *this) {
 
   List<AbnormalStateContainer::AbstateElementInObject> listAbstateObject; // esi
-  AbnormalStateContainer::AbstateElementInObject abstateElement; // edi
-
-  int innerIndex; // eax
-  int outerIndex; // ecx
 
   this->asl_Element = new AbnormalStateContainer::AbstateElementInObject[0x1C);
                                                                          
@@ -14,36 +10,8 @@ void __thiscall AbnormalStateContainer::AbstateListInObject::AbstateListInObject
 
   List<AbnormalStateContainer::AbstateElementInObject>::l_MakeList(listAbstateObject, 0x1C);
 
-  innerIndex = 0;
-  outerIndex = 2;
-  abstateElement = &this->asl_Element[outerIndex];
+  for (int i = 0; i < listAbstateObject->l_MaxSize; i++) {
+        listAbstateObject->l_Array[i].ls_Content = &this->asl_Element[i];
+    }                                                                  
 
-  do {  
-
-    if ( outerIndex - 2 < listAbstateObject->l_MaxSize) {
-      if (!listAbstateObject->l_Array[innerIndex])
-        listAbstateObject->l_Array[innerIndex].ls_Content = &this->asl_Element[outerIndex - 2]
-    }
-
-    if ( outerIndex - 1 < listAbstateObject->l_MaxSize) {
-      if (listAbstateObject->l_Array[innerIndex + 1] == 0)
-        listAbstateObject->l_Array[innerIndex + 1].ls_Content = &this->asl_Element[outerIndex - 1]
-    }
-
-    if ( outerIndex < listAbstateObject->l_MaxSize) {
-      if (listAbstateObject->l_Array[innerIndex + 2] == 0)
-        listAbstateObject->l_Array[innerIndex + 2].ls_Content = &this->asl_Element[outerIndex]
-    }
-
-    if ( outerIndex + 1 < listAbstateObject->l_MaxSize) {
-      if (listAbstateObject->l_Array[innerIndex + 3] == 0)
-        listAbstateObject->l_Array[innerIndex + 3].ls_Content = &this->asl_Element[outerIndex + 1]
-    }
-
-    innerIndex += 4;
-    outerIndex += 4;
-
-    abstateElement = &this->asl_Element[outerIndex];
-  }
-  while ( outerIndex - 2 < 28 );
 }
